03e70b6fb255c88b5896f042a48d6667dd34c96c
JCMondragon7 <johnathan12907@gmail.com>
2026-01-27 14:49:13 +0000

    funct

--- 2_functions.py
+++ 2_functions.py
@@ -3,7 +3,8 @@
 
 # You should only define the function, you should not call it later.
 
-
+def greet():
+    print('hello world!')
 
 
 
@@ -13,10 +14,11 @@
 # Declare a Function: Create a function called welcome that takes a person's name as an argument. This function should print "Welcome {name}!" whenever it is called.
 
 # Create a Variable: Define a variable named name and store any name of your choice in it.
-
+name = 'Alice'
 # Note: Only define the function and create the variable. Do not call the function afterwards.
 
-
+def welcome(name):
+    print(f'welcome {name}!')
 
 
 
@@ -24,6 +26,13 @@
 # Create Functions Practice #3
 # Declare a function called square, that takes any number as an argument, and each time it is called, it prints the square of that number on the screen (that is, the value to the second power).
 
-# The name of the argument that this function must take is number. Create this variable and assign it any number.
+# The name of the parameter that this function must take is number. Create this variable and assign it any number.
 
 # You should only define the function and create the variable, you should not call the function afterwards.
+
+def square(x):
+    z = x * x
+    return z
+
+print(square(2))
+print(square(4))
